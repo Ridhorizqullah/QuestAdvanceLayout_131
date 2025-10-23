c51ddddb4fcd4fb954523f1a4859013a8aff5473
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-23 16:11:49 +0700

    menampilkan strings nama

--- app/src/main/java/com/example/pertemuan4/ui.kt
+++ app/src/main/java/com/example/pertemuan4/ui.kt
@@ -47,10 +47,23 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                     .padding(5.dp)
             )
 
+            spacer(modifier = Modifier.width(30.dp))
+
+            Column {
+                Text(
+                    text = stringResource(id = R.string.nama),
+                    fontSize = 30.sp,
+                    fontFamily = FontFamily.Cursive,
+                    color = Color.White,
+                    modifier = Modifier.padding(top = 25.dp)
+                )
 
 
 
-        }
+
+
+
+            }
 
 }
 
