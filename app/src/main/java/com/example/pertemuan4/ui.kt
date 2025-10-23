a643f8d908f5dec2114c7916d5b5aa9a958881ff
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-23 18:42:30 +0700

    Tambahkan Column dengan Text untuk nama pada Card 3

--- app/src/main/java/com/example/pertemuan4/ui.kt
+++ app/src/main/java/com/example/pertemuan4/ui.kt
@@ -154,6 +154,18 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                         .size(100.dp)
                         .padding(5.dp)
                 )
+                Spacer(modifier = modifier.width(30.dp))
+
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.nama2),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 25.dp)
+                    )
+
+                }
             }
         }
 
